505f4f30a131f7ef202541218626cc9fea2e724e
Igor Zibarev <zibarev.i@gmail.com>
2016-09-13 23:09:09 +0700

    Add package doc

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ slug := "printing-array-elements-with-comma-delimiters"
 kata, _, err := cw.Katas.GetKata(slug)
 if err != nil {
     fmt.Printf("Error: %v\n\n", err)
-    } else {
+} else {
     kataJSON, _ := json.MarshalIndent(kata, "", "  ")
-	fmt.Printf("Kata:\n%s\n\n", kataJSON)
+    fmt.Printf("Kata:\n%s\n\n", kataJSON)
 }
 ```
 
--- codewars.go
+++ codewars.go
@@ -1,3 +1,18 @@
+// Package codewars implements the client for Codewars API.
+//
+// As a simple example:
+//
+//  token := "some-api-token"
+//  cw := codewars.NewClient(token)
+//
+//  slug := "printing-array-elements-with-comma-delimiters"
+//  kata, _, err := cw.Katas.GetKata(slug)
+//  if err != nil {
+//      fmt.Printf("Error: %v\n\n", err)
+//  } else {
+//      kataJSON, _ := json.MarshalIndent(kata, "", "  ")
+//      fmt.Printf("Kata:\n%s\n\n", kataJSON)
+//  }
 package codewars
 
 import (
